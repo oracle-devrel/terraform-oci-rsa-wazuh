452c496ca8f2da3e7a2410c5fb6c1f1bf7caf4da
Leo Siow <leo.siow@oracle.com>
2021-07-19 13:34:17 -0700

    implement writing yml files with passwords

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,18 @@ output "wazuh_cluster_ip" {
 output "wazuh_subnet_cidr" {
   value = oci_core_subnet.wazuh_subnet.cidr_block
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Return the passwords for ElasticSearch and Wazuh
+# ---------------------------------------------------------------------------------------------------------------------
+output "wazuh_password" {
+  value = sha256(random_password.wazuh_password.result)
+}
+
+output "opendistro_kibana_password" {
+  value = random_password.opendistro_kibana_password.result
+}
+
+output "opendistro_admin_password" {
+  value = random_password.opendistro_admin_password.result
+}
