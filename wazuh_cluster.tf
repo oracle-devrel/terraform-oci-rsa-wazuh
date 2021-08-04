f07f172eaa7773dcb80aafd01f5e375032c979bb
sskarnaw <sakshi.karnawat@oracle.com>
2021-08-04 12:02:15 -0700

    Adding support for more secure password

--- wazuh_cluster.tf
+++ wazuh_cluster.tf
@@ -118,7 +118,8 @@ resource "oci_load_balancer_backend" "wazuh_cluster_worker_backends" {
 resource "random_password" "wazuh_password" {
   length           = 16
   special          = true
-  override_special = "_%@"
+  number           = true
+  override_special = "_%@!&*$#^-()"
   min_upper        = 2
   min_lower        = 2
 }
