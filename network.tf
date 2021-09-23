b5314e28318894dc137d4f7b3d96feec0a7eb884
Arjun Patel <arjun.patel@oracle.com>
2021-09-23 13:07:16 -0400

    CAAS-603 removed network formatting

--- network.tf
+++ network.tf
@@ -22,9 +22,9 @@ resource "oci_core_subnet" "wazuh_subnet" {
 }
 
 resource "oci_core_security_list" "wazuh_security_list" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = var.vcn_id
-  display_name   = "Wazuh Security List"
+  compartment_id      = var.compartment_ocid
+  vcn_id              = var.vcn_id
+  display_name        = "Wazuh Security List"
   freeform_tags = {
     "Description" = "Wazuh security lists"
     "Function"    = "Ingress and egress rules for wazuh service"
@@ -32,20 +32,20 @@ resource "oci_core_security_list" "wazuh_security_list" {
 
   egress_security_rules {
     destination = var.egress_security_rules_destination
-    protocol    = var.egress_security_rules_protocol
-    stateless   = var.egress_security_rules_stateless
+    protocol = var.egress_security_rules_protocol
+    stateless = var.egress_security_rules_stateless
     tcp_options {
       max = var.egress_security_rules_tcp_options_destination_port_range_max
       min = var.egress_security_rules_tcp_options_destination_port_range_min
     }
   }
-  dynamic "ingress_security_rules" {
-    for_each = var.wazuh_ingress_ports
+  dynamic ingress_security_rules {
+    for_each = var.wazuh_ingress_ports 
     content {
-      protocol    = var.wazuh_ingress_security_rules_protocol
-      source      = data.oci_core_vcn.vcn.cidr_blocks[0]
+      protocol = var.wazuh_ingress_security_rules_protocol
+      source = data.oci_core_vcn.vcn.cidr_blocks[0]
       description = var.wazuh_ingress_security_rules_description
-      stateless   = var.wazuh_ingress_security_rules_stateless
+      stateless = var.wazuh_ingress_security_rules_stateless
       tcp_options {
         max = ingress_security_rules.value
         min = ingress_security_rules.value
@@ -55,30 +55,30 @@ resource "oci_core_security_list" "wazuh_security_list" {
 }
 
 resource "oci_core_security_list" "kibana_security_list" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = var.vcn_id
-  display_name   = "Kibana Security List"
+  compartment_id      = var.compartment_ocid
+  vcn_id              = var.vcn_id
+  display_name        = "Kibana Security List"
   freeform_tags = {
     "Description" = "Kibana security lists"
     "Function"    = "Ingress rule for kibana web service"
   }
 
   ingress_security_rules {
-    protocol    = var.kibana_ingress_security_rules_protocol
-    source      = data.oci_core_vcn.vcn.cidr_blocks[0]
+    protocol = var.kibana_ingress_security_rules_protocol
+    source = data.oci_core_vcn.vcn.cidr_blocks[0]
     description = var.kibana_ingress_security_rules_description
-    stateless   = var.kibana_ingress_security_rules_stateless
+    stateless = var.kibana_ingress_security_rules_stateless
     tcp_options {
-      max = var.kibana_tcp_port
+      max = var.kibana_tcp_port 
       min = var.kibana_tcp_port
     }
   }
 }
 
 resource "oci_core_security_list" "es_cluster_security_list" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = var.vcn_id
-  display_name   = "ES cluster Security List"
+  compartment_id      = var.compartment_ocid
+  vcn_id              = var.vcn_id
+  display_name        = "ES cluster Security List"
   freeform_tags = {
     "Description" = "ES cluster security lists"
     "Function"    = "Ingress rule for elastic search cluster"
@@ -97,9 +97,9 @@ resource "oci_core_security_list" "es_cluster_security_list" {
 }
 
 resource "oci_core_security_list" "es_api_security_list" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = var.vcn_id
-  display_name   = "ES API Security List"
+  compartment_id      = var.compartment_ocid
+  vcn_id              = var.vcn_id
+  display_name        = "ES API Security List"
   freeform_tags = {
     "Description" = "ES API security lists"
     "Function"    = "Ingress rule for elastic search service API"
@@ -111,7 +111,7 @@ resource "oci_core_security_list" "es_api_security_list" {
     description = var.es_api_ingress_security_rules_description
     stateless   = var.es_api_ingress_security_rules_stateless
     tcp_options {
-      max = var.es_api_tcp_port
+      max = var.es_api_tcp_port 
       min = var.es_api_tcp_port
     }
   }
@@ -119,25 +119,25 @@ resource "oci_core_security_list" "es_api_security_list" {
 
 resource "oci_dns_zone" "wazuh_dns_zone" {
   compartment_id = var.compartment_ocid
-  name           = var.dns_zone_name
-  zone_type      = var.dns_zone_type
-  scope          = var.dns_zone_scope
-  view_id        = data.oci_dns_views.primary_view.views[0].id
+  name = var.dns_zone_name
+  zone_type = var.dns_zone_type
+  scope = var.dns_zone_scope
+  view_id = data.oci_dns_views.primary_view.views[0].id
 }
 
 resource "oci_dns_rrset" "wazuhlb_dns_record" {
-  domain          = "${var.wazuh_record_items_domain}.${var.dns_zone_name}"
-  rtype           = var.wazuh_record_items_rtype
+  domain = "${var.wazuh_record_items_domain}.${var.dns_zone_name}"
+  rtype = var.wazuh_record_items_rtype
   zone_name_or_id = oci_dns_zone.wazuh_dns_zone.id
-  compartment_id  = var.compartment_ocid
+  compartment_id = var.compartment_ocid
 
   items {
     domain = "${var.wazuh_record_items_domain}.${var.dns_zone_name}"
-    rdata  = oci_load_balancer.wazuh_cluster_load_balancer.ip_address_details[0].ip_address
-    rtype  = var.wazuh_record_items_rtype
-    ttl    = var.wazuh_record_items_ttl
+    rdata = oci_load_balancer.wazuh_cluster_load_balancer.ip_address_details[0].ip_address
+    rtype = var.wazuh_record_items_rtype
+    ttl = var.wazuh_record_items_ttl
   }
-  scope   = var.dns_zone_scope
+  scope = var.dns_zone_scope
   view_id = data.oci_dns_views.primary_view.views[0].id
 }
 
